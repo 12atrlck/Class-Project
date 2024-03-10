93795048f61c78a36d6b7fb1ab21d3739df32b9f
Pato <158470923+12atrlck@users.noreply.github.com>
2024-03-10 22:59:40 +0100

    javascript button

---
+++ main.js
@@ -0,0 +1,22 @@
+function checkOdd() {
+    let result = document.getElementById('result');
+        //this line access the input 
+    let user = document.getElementById('inputNo');
+
+    //this line access the value input
+    let userInput = user.value;
+
+    let numberToBeChecked = parseInt(userInput)
+
+    if(typeof numberToBeChecked === 'number'){
+       if(numberToBeChecked < 18){
+       // document.getElementById().innerHTML
+       result.innerHTML = 'you are not allowed to take alchol'
+       } else{
+        result.innerHTML = 'you are allowed to take alchol'
+       }
+    } else{
+       result.innerHTML = ('You have entered an invalid character')
+
+    }
+}
